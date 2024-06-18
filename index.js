9b165955d7ec4bd1cced0abbc4ed705bc91a0450
Amirhossein Olyaei <amirholyaei@gmail.com>
2024-06-17 21:57:17 -0400

    "Updated course info, assignment group, and learner submissions with new data."

--- index.js
+++ index.js
@@ -1,30 +1,74 @@
 const courseInfo = {
-  id: 1,
+  id: 451,
   name: "Introduction to JavaScript",
 };
 
 const assignmentGroup = {
-  id: 1,
-  name: "Week 1 Assignments",
-  course_id: 1,
-  group_weight: 1.0,
+  id: 12345,
+  name: "Fundamentals of JavaScript",
+  course_id: 451,
+  group_weight: 25,
   assignments: [
     {
       id: 1,
-      name: "Assignment 1",
-      points_possible: 10,
-      due_at: "2023-03-15T23:59:59Z",
+      name: "Declare a Variable",
+      due_at: "2023-01-25",
+      points_possible: 50,
+    },
+    {
+      id: 2,
+      name: "Write a Function",
+      due_at: "2023-02-27",
+      points_possible: 150,
+    },
+    {
+      id: 3,
+      name: "Code the World",
+      due_at: "3156-11-15",
+      points_possible: 500,
     },
   ],
 };
 
 const learnerSubmissions = [
   {
-    learner_id: 1,
+    learner_id: 125,
     assignment_id: 1,
     submission: {
-      submitted_at: "2023-03-16T12:34:56Z",
-      score: 8,
+      submitted_at: "2023-01-25",
+      score: 47,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-02-12",
+      score: 150,
+    },
+  },
+  {
+    learner_id: 125,
+    assignment_id: 3,
+    submission: {
+      submitted_at: "2023-01-25",
+      score: 400,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 1,
+    submission: {
+      submitted_at: "2023-01-24",
+      score: 39,
+    },
+  },
+  {
+    learner_id: 132,
+    assignment_id: 2,
+    submission: {
+      submitted_at: "2023-03-07",
+      score: 140,
     },
   },
 ];
